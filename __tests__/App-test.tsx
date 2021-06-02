398b78f1e1a03c9cde0877223b3952a60523314d
Fenér Norbert <feher.norbert@webdream.hu>
2021-06-02 16:54:24 +0200

    clean the app and add a simple test

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -2,13 +2,15 @@
  * @format
  */
 
-import 'react-native';
 import React from 'react';
 import App from '../App';
+import {render} from "@testing-library/react-native";
 
-// Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+describe('App test', () => {
+  test('render correctly', () => {
+    const wrapper = render(<App />);
+    const testText = wrapper.getByTestId('test-text');
+  });
 });
